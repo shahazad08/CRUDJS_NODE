89efcbbbce764b38e4349d982f1c9c4b1f5c224d
shahazad08 <sk.shahazad@gmail.com>
2021-11-20 15:01:55 +0530

    Added: INstalled mongo db serv, and created a connection

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const url='mongodb://localhost/AlienDBex'
+
+const app=express()
+
+mongoose.connect(url, {useNewUrlParser:true})
+const con=mongoose.connection
+
+con.on('open', function(){
+    console.log('connected...');
+})
+
+app.listen(9000, ()=> {
+    console.log("Server Started");
+})
